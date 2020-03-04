4fae718aea0e64d67b1909d6df49d36593a0d980
GULAM AHMED RAZA <54493079+glmoo7git@users.noreply.github.com>
2020-03-05 02:58:09 +0530

    Update oop.py

--- Object-Oriented/3-Class-Static-Methods/oop.py
+++ Object-Oriented/3-Class-Static-Methods/oop.py
@@ -15,12 +15,15 @@ class Employee:
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
-    def apply_raise(self):
+    def apply_raise(self):       #this is a regular method
         self.pay = int(self.pay * self.raise_amt)
 
     @classmethod
     def set_raise_amt(cls, amount):
         cls.raise_amt = amount
+        
+#  regular method we called this instance variable self and there's a common
+# convention for class variables too and that is CLS
 
     @classmethod
     def from_string(cls, emp_str):
@@ -37,11 +40,23 @@ class Employee:
 # instance or class variable so instead 
 
     @staticmethod                   
-    def is_workday(day):
-        if day.weekday() == 5 or day.weekday() == 6:
-            return False
-        return True
-
+    def is_workday(day):                                     #remember static methods don't take the
+        if day.weekday() == 5 or day.weekday() == 6:         #instance or the class as the first
+            return False                                     #argument so we can just pass in the
+        return True                                          #arguments that we want to work with
+    
+# now sometimes people write regular methods or class methods that actually should be static
+# methods and usually a giveaway that a method should be a static method is if
+# you don't access the instance or the class anywhere within the function so
+# say that I had this class method up here     
+# @classmethod
+#     def from_string(cls, emp_str):
+#         first, last, pay = emp_str.split('-')
+#         return cls(first, last, pay)
+# you can see that I'm using that cls variable there but if I wasn't using it anywhere within that method then it
+# probably doesn't need to be a class method and the same with regular methods
+# if you're not using that self variable then  you probably want to
+# check and see if that would be appropriate to use a static method 
 
 emp_1 = Employee('Corey', 'Schafer', 50000)
 emp_2 = Employee('Test', 'Employee', 60000)
